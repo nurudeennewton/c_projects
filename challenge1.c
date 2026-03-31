c9bda8ccf86915730daf97626426e34241feeafd
nurudeen-newton <nurudeennewton@gmail.com>
2026-03-31 18:48:47 +0100

    phase 1- variables and data types

---
+++ challenge1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+	char name = 'N';
+	int age = 24;
+	float height = 1.96;
+	printf("Name: %C\n", name);
+	printf("Age: %d\n", age);
+	printf("Height: %.2f\n", height);
+	return 0;
+}
---
+++ variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    float gpa = 3.75;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("GPA: %.2f\n", gpa);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
